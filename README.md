f32d4470fe91ed94e85f913b6e816d2b05bd44cc
cballesteros10 <camilaballesteros123@gmail.com>
2023-11-13 17:27:08 -0700

    Primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# proyecto_vehiculos
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/proyecto_vehiculos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.proyecto_vehiculos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
