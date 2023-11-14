39f9e5f9fea1faa24cf88a364431eae80be72c63
cballesteros10 <camilaballesteros123@gmail.com>
2023-11-14 14:45:57 -0700

    Edición de vistas

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,27 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Prueba',
+    return const MaterialApp(
+      title: 'MyCarApp',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              OpcionesPantallas()
-            ],
-          ),
-          centerTitle: true,
-          flexibleSpace: Container(
+        appBar: MiAppBar(),
+        body: MisTabs()
+      ));
+  }
+}
+
+class MiAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MiAppBar({Key? key}) : super(key: key);
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text("MyCarApp"),
+      flexibleSpace: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: [
@@ -32,34 +40,259 @@ class MainApp extends StatelessWidget {
             end: Alignment.bottomRight,
           ),
         ),
-      )),
-        body: const Center(
-          // MiInfo()
+      ),
+      centerTitle: true,
+    );
+  }
+}
+
+class MisTabs extends StatefulWidget {
+  const MisTabs({super.key});
+
+  @override
+  MisTabsState createState() => MisTabsState();
+}
+
+class MisTabsState extends State<MisTabs> {
+  List<String> data = ['Vehículos', 'Gastos', 'Consultas'];
+  int initPosition = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: CustomTabView(
+          initPosition: initPosition,
+          itemCount: data.length,
+          tabBuilder: (context, index) => Tab(text: data[index]),
+          pageBuilder: (context, index) {
+            switch (index) {
+              case 0:
+                return ListaVehiculos();
+              case 1:
+                return ListaGastos();
+              case 2:
+                return ListaConsultas();
+              default:
+                return Container();
+            }
+          }
         ),
+      ));
+  }
+}
+
+class CustomTabView extends StatefulWidget {
+  const CustomTabView({
+    super.key,
+    required this.itemCount,
+    required this.tabBuilder,
+    required this.pageBuilder,
+    this.stub,
+    this.onPositionChange,
+    this.onScroll,
+    this.initPosition,
+  });
+
+  final int itemCount;
+  final IndexedWidgetBuilder tabBuilder;
+  final IndexedWidgetBuilder pageBuilder;
+  final Widget? stub;
+  final ValueChanged<int>? onPositionChange;
+  final ValueChanged<double>? onScroll;
+  final int? initPosition;
+
+  @override
+  CustomTabsState createState() => CustomTabsState();
+}
+
+class CustomTabsState extends State<CustomTabView>
+    with TickerProviderStateMixin {
+  late TabController controller;
+  late int _currentCount;
+  late int _currentPosition;
+
+  @override
+  void initState() {
+    _currentPosition = widget.initPosition ?? 0;
+    controller = TabController(
+      length: widget.itemCount,
+      vsync: this,
+      initialIndex: _currentPosition,
+    );
+    controller.addListener(onPositionChange);
+    _currentCount = widget.itemCount;
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(CustomTabView oldWidget) {
+    if (_currentCount != widget.itemCount) {
+      controller.removeListener(onPositionChange);
+      controller.dispose();
+
+      if (widget.initPosition != null) {
+        _currentPosition = widget.initPosition!;
+      }
+
+      if (_currentPosition > widget.itemCount - 1) {
+        _currentPosition = widget.itemCount - 1;
+        _currentPosition = _currentPosition < 0 ? 0 : _currentPosition;
+        if (widget.onPositionChange is ValueChanged<int>) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            if (mounted && widget.onPositionChange != null) {
+              widget.onPositionChange!(_currentPosition);
+            }
+          });
+        }
+      }
+
+      _currentCount = widget.itemCount;
+      setState(() {
+        controller = TabController(
+          length: widget.itemCount,
+          vsync: this,
+          initialIndex: _currentPosition,
+        );
+        controller.addListener(onPositionChange);
+      });
+    } else if (widget.initPosition != null) {
+      controller.animateTo(widget.initPosition!);
+    }
+
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(onPositionChange);
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.itemCount < 1) return widget.stub ?? Container();
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          alignment: Alignment.center,
+          child: TabBar(
+            isScrollable: true,
+            controller: controller,
+            labelColor: Theme.of(context).primaryColor,
+            unselectedLabelColor: Theme.of(context).hintColor,
+            indicator: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Theme.of(context).primaryColor,
+                  width: 2,
+                ),
+              ),
+            ),
+            tabs: List.generate(
+              widget.itemCount,
+              (index) => widget.tabBuilder(context, index),
+            ),
+          ),
+        ),
+        Expanded(
+          child: TabBarView(
+            controller: controller,
+            children: List.generate(
+              widget.itemCount,
+              (index) => widget.pageBuilder(context, index),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void onPositionChange() {
+    if (!controller.indexIsChanging) {
+      _currentPosition = controller.index;
+      if (widget.onPositionChange is ValueChanged<int>) {
+        widget.onPositionChange!(_currentPosition);
+      }
+    }
+  }
+}
+
+class ListaVehiculos extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: const [
+          ListTile(title: Text('Vehículo 1')),
+          ListTile(title: Text('Vehículo 2')),
+          // ... más elementos según tus datos
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Lógica para manejar el botón de agregar aquí
+        },
+        child: const Icon(Icons.add_box_rounded),
       ),
     );
   }
 }
 
-class OpcionesPantallas extends StatefulWidget {
-  const OpcionesPantallas({super.key});
 
-  @override
-  State<OpcionesPantallas> createState() => _OpcionesPantallasState();
-}
-
-class _OpcionesPantallasState extends State<OpcionesPantallas> {
+class ListaGastos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SegmentedButton(
-      showSelectedIcon: false,
-      multiSelectionEnabled: false,
-      segments: const [
-      ButtonSegment(icon: Icon(Icons.car_crash), value: 'Vehiculos', label: Text('Vehiculos')),
-      ButtonSegment(icon: Icon(Icons.money), value: 'Gastos', label: Text('Gastos')),
-      ButtonSegment(icon: Icon(Icons.woman), value: 'Consultas', label: Text('Consultas'))
-    ], 
-      selected: const {2});
+    return Scaffold(
+      body: ListView(
+        children: const [
+          ListTile(title: Text('Gasto 1')),
+          ListTile(title: Text('Gasto 2')),
+          // ... más elementos según tus datos
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Lógica para manejar el botón de agregar aquí
+        },
+        child: const Icon(Icons.add_box_rounded),
+      ),
+    );
+  }
+}
+
+class ListaConsultas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              const Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.description),
+                    hintText: 'Buscar consultas...',
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {
+                  print('Prueba');
+                },
+              ),
+            ],
+          ),
+        ),
+        // Agrega más elementos según tus datos
+      ],
+    );
   }
 }
 
